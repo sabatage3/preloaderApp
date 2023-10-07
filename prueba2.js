a56bc907062803a152ac290ad59c54192ca3a693
sabatage3 <info@rubenromero.co>
2023-10-06 19:20:09 -0500

    Update prueba2.js LOGO

--- prueba2.js
+++ prueba2.js
@@ -1,50 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Spinner con Logo</title>
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            min-height: 100vh;
-            background-color: black;
-        }
+// Crear un div para el fondo negro del spinner
+var overlay = document.createElement("div");
+overlay.style.position = "fixed";
+overlay.style.top = "0";
+overlay.style.left = "0";
+overlay.style.width = "100%";
+overlay.style.height = "100%";
+overlay.style.backgroundColor = "rgba(0, 0, 0, 1)";
+overlay.style.zIndex = "9999";
 
-        .spinner-container {
-            display: flex;
-            align-items: center;
-        }
+// Crear el contenedor del spinner
+var spinnerContainer = document.createElement("div");
+spinnerContainer.style.position = "absolute";
+spinnerContainer.style.top = "50%";
+spinnerContainer.style.left = "50%";
+spinnerContainer.style.transform = "translate(-50%, -50%)";
+spinnerContainer.style.textAlign = "center";
 
-        .spinner {
-            width: 24px;
-            height: 24px;
-            border: 4px solid transparent;
-            border-top: 4px solid #FEA200;
-            border-radius: 50%;
-            animation: spin 1s linear infinite;
-            margin-right: 10px;
-        }
+// Crear el spinner de tipo barsfade
+var spinner = document.createElement("div");
+spinner.className = "spinner-barsfade";
 
-        @keyframes spin {
-            0% { transform: rotate(0deg); }
-            100% { transform: rotate(360deg); }
-        }
+// Crear la imagen del logo
+var logo = document.createElement("img");
+logo.src = "https://firebasestorage.googleapis.com/v0/b/energie-check-24.appspot.com/o/LogoBlanco_EnergieCheck24.svg?alt=media&token=dab4a948-08f8-4801-83be-082f09d9c093";
+logo.alt = "Logo";
 
-        .logo {
-            max-width: 100%;
-            max-height: 100%;
-        }
-    </style>
-</head>
-<body>
-    <div class="spinner-container">
-        <div class="spinner"></div>
-        <img class="logo" src="https://firebasestorage.googleapis.com/v0/b/energie-check-24.appspot.com/o/LogoBlanco_EnergieCheck24.svg?alt=media&token=dab4a948-08f8-4801-83be-082f09d9c093" alt="Logo">
-    </div>
-</body>
-</html>
+// Agregar el spinner y la imagen al contenedor
+spinnerContainer.appendChild(spinner);
+spinnerContainer.appendChild(logo);
+
+// Agregar el contenedor al overlay
+overlay.appendChild(spinnerContainer);
+
+// Agregar el overlay al cuerpo del documento
+document.body.appendChild(overlay);
